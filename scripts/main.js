fdf32fc20ce61e5ca48122b28f8078426b88289f
Pow_point <88034480+Soulvdark@users.noreply.github.com>
2024-02-02 18:10:25 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,27 @@
+const tabItem = document.querySelectorAll('.menu__list');
+const tabContent = document.querySelectorAll('.cards');
+
+tabItem.forEach(function (element) {
+	element.addEventListener('click', open);
+});
+
+function open(evt) {
+	const tabTarget = evt.currentTarget;
+	const button = tabTarget.dataset.button;
+
+	tabItem.forEach(function (item) {
+		item.classList.remove('tabs__btn-item--active');
+	});
+
+	tabTarget.classList.add('tabs__btn-item--active');
+
+	tabContent.forEach(function (item) {
+		item.classList.remove('tabs__content-item--active');
+	});
+	document
+		.querySelector(`#${button}`)
+		.classList.add('tabs__content-item--active');
+}
 
 let darkModeEnabled = false;
 
@@ -19,7 +43,5 @@ function theme() {
 		}
 	});
 }
-theme();
 
-console.log(6%3);
-console.log(5/3)
+theme();
