a9ed66b25ad9d8f2ab7f21f3e7ac5f02aef92131
Pow_point <88034480+Soulvdark@users.noreply.github.com>
2024-02-08 21:01:06 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,40 @@
+// let elems = document.querySelectorAll('.card');
+// let text = document.querySelector(".dashboard-project")
+let text = document.querySelector('.dashboard-project__number');
+
+// console.log(card)
+// text.textContent = card;
+// Получаем все элементы с классом "cards"
+let card = document.querySelectorAll('.card');
+function project() {
+	text.textContent = card.length;
+}
+project();
+
 const tabItem = document.querySelectorAll('.menu__list');
 const tabContent = document.querySelectorAll('.cards');
+const activeOnElements = document.querySelectorAll('.active-on');
+const contentItems = document.querySelectorAll('.tabs__content-item');
+
+let darkModeEnabled = false;
+function theme() {
+	const darkModeButton = document.getElementById('darkModeButton');
+	const enableDarkMode = () => {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	};
+	const disableDarkMode = () => {
+		document.documentElement.removeAttribute('data-theme');
+	};
+	darkModeButton.addEventListener('click', () => {
+		darkModeEnabled = !darkModeEnabled;
+
+		if (darkModeEnabled) {
+			enableDarkMode();
+		} else {
+			disableDarkMode();
+		}
+	});
+}
 
 tabItem.forEach(function (element) {
 	element.addEventListener('click', open);
@@ -23,25 +58,12 @@ function open(evt) {
 		.classList.add('tabs__content-item--active');
 }
 
-let darkModeEnabled = false;
-
-function theme() {
-	const darkModeButton = document.getElementById('darkModeButton');
-	const enableDarkMode = () => {
-		document.documentElement.setAttribute('data-theme', 'dark');
-	};
-	const disableDarkMode = () => {
-		document.documentElement.removeAttribute('data-theme');
-	};
-	darkModeButton.addEventListener('click', () => {
-		darkModeEnabled = !darkModeEnabled;
-
-		if (darkModeEnabled) {
-			enableDarkMode();
-		} else {
-			disableDarkMode();
-		}
+window.addEventListener('load', function () {
+	let elementToAnimate = document.querySelector('.anim');
+	elementToAnimate.classList.add('anim');
+	elementToAnimate.addEventListener('animationend', function () {
+		elementToAnimate.classList.remove('anim');
 	});
-}
+});
 
 theme();
